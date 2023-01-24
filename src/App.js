f176415b0ddca9caac1bccd3a0bfa413f952cdc3
aniruddhasuryawanshi3 <aniruddhasuryawanshi3@gmail.com>
2023-01-24 13:06:09 +0900

    Adding final files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
+import HomePage from "./pages/Home.page";
+import MoviePage from "./pages/Movie.page";
+import PlayPage from "./pages/Play.page";
+import { Routes, Route } from "react-router-dom";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = "23950def2b9ed6a132bd7c7a8e7efd61";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<PlayPage />} />
+    </Routes>
   );
 }
 
